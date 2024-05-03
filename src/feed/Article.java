76395d40affa91b5eba21ed49be6f168ad9b88c7
Tomás Peyronel <tpeyronel@gmail.com>
2024-05-03 18:04:40 -0300

    define Article

--- src/feed/Article.java
+++ src/feed/Article.java
@@ -1,5 +1,23 @@
 package feed;
 
 public class Article {
-    // TODO
+    String title;
+    String description;
+    String pubDate;
+    String link;
+
+    Article(String title, String description, String pubDate, String link) {
+        this.title = title;
+        this.description = description;
+        this.pubDate = pubDate;
+        this.link = link;
+    }
+    
+    void print() {
+        System.out.println("Title: " + title);
+        System.out.println("Description: " + description);
+        System.out.println("Publication date: " + pubDate);
+        System.out.println("Link: " + link);
+        System.out.println("********************************************************************************");
+    }
 }
