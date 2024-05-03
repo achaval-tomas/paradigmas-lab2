6f97151e0fc9330db31c00b5f6ac278fd9038aec
Tomás Peyronel <tpeyronel@gmail.com>
2024-05-03 18:05:24 -0300

    implement FeedParser

--- src/feed/FeedParser.java
+++ src/feed/FeedParser.java
@@ -1,18 +1,52 @@
 package feed;
 
+import java.beans.XMLDecoder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public class FeedParser {
 
-    public static List<Article> parseXML(String xmlData) {
-        // TODO
-        return null;
+    public static List<Article> parseXML(String xmlData) throws ParserConfigurationException, SAXException, IOException {
+        List<Article> articles = new ArrayList<>();
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document document = db.parse(new InputSource(new StringReader(xmlData)));
+
+        NodeList items = document.getElementsByTagName("item");
+
+        for (int i = 0; i < items.getLength(); i++) {
+            Element item = (Element)items.item(i);
+            String title = item.getElementsByTagName("title").item(0).getTextContent().trim();
+            String description = item.getElementsByTagName("dcterms:alternative").item(0).getTextContent().trim();
+            String link = item.getElementsByTagName("link").item(0).getTextContent().trim();
+            String pubDate = item.getElementsByTagName("pubDate").item(0).getTextContent().trim();
+
+            Article article = new Article(title, description, link, pubDate);
+            article.print();
+
+            articles.add(article);
+        }
+
+        return articles;
     }
 
     public static String fetchFeed(String feedURL) throws MalformedURLException, IOException, Exception {
@@ -22,8 +56,8 @@ public class FeedParser {
 
         connection.setRequestMethod("GET");
         connection.setRequestProperty("Content-Type", "application/json");
-        
-        // TODO: Cambiar el user-agent al nombre de su grupo. 
+
+        // TODO: Cambiar el user-agent al nombre de su grupo.
         // Si todos los grupos usan el mismo user-agent, el servidor puede bloquear las solicitudes.
         connection.setRequestProperty("User-agent", "lab_paradigmas");
         connection.setConnectTimeout(5000);
