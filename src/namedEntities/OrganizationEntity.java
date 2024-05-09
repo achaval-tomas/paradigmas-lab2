8d1e401d33780fcf07ffc984ae6334f2d0aae020
achaval-tomas <tomiachaval2013@gmail.com>
2024-05-09 18:59:39 -0300

    Rename file

--- src/namedEntities/OrganizationEntity.java
+++ src/namedEntities/OrganizationEntity.java
@@ -2,12 +2,12 @@ package namedEntities;
 
 import java.util.List;
 
-public class OranizationEntity extends NamedEntity {
+public class OrganizationEntity extends NamedEntity {
     private final String orgName;
     private final String type; // ONG, SA, SRL
 
-    public OranizationEntity(String label, List<String> topics, List<String> keywords,
-                             String orgName, String type) {
+    public OrganizationEntity(String label, List<String> topics, List<String> keywords,
+                              String orgName, String type) {
         super(label, topics, keywords);
         this.orgName = orgName;
         this.type = type;
