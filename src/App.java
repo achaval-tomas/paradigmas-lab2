2f27c7473eaa670916ddae35aa38f731cd69378f
achaval-tomas <tomiachaval2013@gmail.com>
2024-05-23 11:08:44 -0300

    printHelp is now in a better place 🙏
    Co-authored-by: Tomás Maraschio <tomimara52@users.noreply.github.com>

--- src/App.java
+++ src/App.java
@@ -31,18 +31,13 @@ public class App {
         }
 
         UserInterface ui = new UserInterface();
-        Config config = ui.handleInput(args, heuristics);
+        Config config = ui.handleInput(args, feedsDataArray, heuristics);
 
-        run(config, feedsDataArray, heuristics);
+        run(config, feedsDataArray);
     }
 
-    private static void run(Config config, List<FeedsData> feedsDataArray, List<Heuristic> heuristics)
+    private static void run(Config config, List<FeedsData> feedsDataArray)
             throws ParserConfigurationException, IOException, SAXException {
-
-        if (config.getPrintHelp()) {
-            printHelp(feedsDataArray, heuristics);
-            return;
-        }
 
         if (feedsDataArray == null || feedsDataArray.isEmpty()) {
             System.out.println("No feeds data found");
@@ -133,33 +128,6 @@ public class App {
                 System.out.printf("        %s (%d)\n", entity.getLabel(), stats.get(entity));
             }
         }
-    }
-
-
-    // TODO: Maybe relocate this function where it makes more sense
-    private static void printHelp(List<FeedsData> feedsDataArray, List<Heuristic> heuristics) {
-        System.out.println("Usage: make run ARGS=\"[OPTION]\"");
-        System.out.println("Options:");
-        System.out.println("  -h, --help: Show this help message and exit");
-        System.out.println("  -f, --feed <feedKey>:                Fetch and process the feed with");
-        System.out.println("                                       the specified key");
-        System.out.println("                                       Available feed keys are: ");
-        for (FeedsData feedData : feedsDataArray) {
-            System.out.println("                                       " + feedData.getLabel());
-        }
-        System.out.println("  -ne, --named-entity <heuristicName>: Use the specified heuristic to extract");
-        System.out.println("                                       named entities");
-
-        System.out.println("        Available heuristic names are: ");
-        for (Heuristic h : heuristics) {
-            System.out.printf("                %s, \"%s\": %s\n", h.getShortName(), h.getLongName(), h.getDescription());
-        }
-
-        System.out.println("  -pf, --print-feed:                   Print the fetched feed");
-        System.out.println("  -sf, --stats-format <format>:        Print the stats in the specified format");
-        System.out.println("                                       Available formats are: ");
-        System.out.println("                                       cat: Category-wise stats");
-        System.out.println("                                       topic: Topic-wise stats");
     }
 
     private static List<NamedEntity> extractNamedEntities(List<String> candidates) throws IOException {
--- src/utils/Config.java
+++ src/utils/Config.java
@@ -5,25 +5,19 @@ import namedEntities.heuristics.Heuristic;
 import java.util.List;
 
 public class Config {
-    private boolean printHelp;
     private boolean printFeed = false;
     private boolean computeNamedEntities = false;
     private List<FeedsData> feedsData;
     private Heuristic heuristic;
     private StatisticsFormat statsFormat;
 
-    public Config(boolean printHelp, boolean printFeed, boolean computeNamedEntities, List<FeedsData> feedsData,
+    public Config(boolean printFeed, boolean computeNamedEntities, List<FeedsData> feedsData,
                   Heuristic heuristic, StatisticsFormat statsFormat) {
-        this.printHelp = printHelp;
         this.printFeed = printFeed;
         this.computeNamedEntities = computeNamedEntities;
         this.feedsData = feedsData;
         this.heuristic = heuristic;
         this.statsFormat = statsFormat;
-    }
-
-    public boolean getPrintHelp() {
-        return printHelp;
     }
 
     public boolean getPrintFeed() {
